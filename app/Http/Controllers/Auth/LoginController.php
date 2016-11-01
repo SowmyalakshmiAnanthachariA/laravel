69e753a953566fbc56ea43f5e056c54551ecf119
SowmyalakshmiAnanthachariA <sowmyalakshmimath@gmail.com>
2016-11-01 14:15:27 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,11 @@ class LoginController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
     }
+    public function index(){
+    	return 'hello world!';
+    }
+    public function contact(){
+    	return 'Contact Me';
+    }
+    
 }
